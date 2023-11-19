5beb4902463720fc1a0accb801e3a8ab13d17ae2
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-19 15:47:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function toCase(text) {
   // write your code here
+
+	let s1=text.toLowerCase();
+	let s2=text.toUpperCase();
+
+	let s="";
+	s+=s1;
+	s+="-";
+	s+=s2;
+
+	return s;
 }
 
 // DO not change the code below
